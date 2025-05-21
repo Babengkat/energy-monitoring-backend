53a258404524759e52ca71358155ae7a258374cc
Babengkat <autentico.ira@gmail.com>
2025-05-22 03:22:36 +0800

    Fix

--- api/views.py
+++ api/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny
-
-
 from rest_framework import viewsets
 from .models import EnergyReading
 from .serializers import EnergyReadingSerializer
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 class EnergyReadingViewSet(viewsets.ModelViewSet):
     queryset = EnergyReading.objects.all().order_by('-timestamp')
     serializer_class = EnergyReadingSerializer
-    permission_classes = [AllowAny] 
-
--- backend/settings.py
+++ backend/settings.py
@@ -35,15 +35,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.permissions.AllowAny',
-    ),
-    # Other settings...
-}
-
 # Application definition
 
 INSTALLED_APPS = [
